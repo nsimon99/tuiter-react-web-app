43643af9a6547a1c9696ddc13fbfb5cc2d4b2d0a
nsimon99 <simon.n@husky.neu.edu>
2022-10-13 17:15:21 -0400

    labs

---
+++ src/labs/index.js
@@ -0,0 +1,14 @@
+import Assignment6 from "./a6";
+import nav from "../nav";
+
+import TodoList from "./a6/todo/todo-list";
+function Labs() {
+    return (
+        <div>
+            <nav/>
+            <Assignment6/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Labs;
