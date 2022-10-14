f27df2f5a67ab7884ab3e1d80c77b0b2224c071a
nsimon99 <simon.n@husky.neu.edu>
2022-10-14 14:22:28 -0400

    added alt to image

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -27,7 +27,7 @@ const ExploreComponent = () => {
                 </li>
             </ul>
             <div className="position-relative mb-2">
-                <img src="/image/starship.webp" className="w-100"/>
+                <img src="/image/starship.webp" className="w-100" alt={""}/>
                 <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
