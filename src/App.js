c0c9cd20eb1492e5ed5e40b4d68b1c587ee11c4b
nsimon99 <simon.n@husky.neu.edu>
2022-10-19 11:59:11 -0400

    lab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import Assignment6 from "./labs/a6";
+import Assignment7 from "./labs/a7";
 
 function App() {
     return (
@@ -14,6 +16,10 @@ function App() {
                            element={<HelloWorld/>}/>
                     <Route path="/tuiter"
                            element={<Tuiter/>}/>
+                    <Route path="/a6"
+                           element={<Assignment6/>}/>
+                    <Route path="/a7"
+                           element={<Assignment7/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
